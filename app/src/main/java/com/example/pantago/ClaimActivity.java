23a97447441c910a44dba929e45acd8fcaff474d
Victor Andersen <s194577@student.dtu.dk>
2020-06-15 16:45:26 +0200

    Claim and Upload Activity UI additions

--- app/src/main/java/com/example/pantago/ClaimActivity.java
+++ app/src/main/java/com/example/pantago/ClaimActivity.java
@@ -3,12 +3,19 @@ package com.example.pantago;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;;
 
 public class ClaimActivity extends AppCompatActivity {
+
+    public static boolean claimed;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_claim);
+        Button claim = (Button) findViewById(R.id.buttonClaim);
     }
+
+
+
 }
--- app/src/main/java/com/example/pantago/UploadActivity.java
+++ app/src/main/java/com/example/pantago/UploadActivity.java
@@ -3,12 +3,43 @@ package com.example.pantago;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.lang.invoke.ConstantCallSite;
 
 public class UploadActivity extends AppCompatActivity {
+
+    private Button uploadButton;
+    private EditText amountText;
+    private EditText commentText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload);
+        uploadButton = (Button) findViewById(R.id.buttonUpload);
+        amountText = (EditText) findViewById(R.id.number_of_objects);
+        commentText = (EditText) findViewById(R.id.upload_comment);
+
+        uploadButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                try{
+                    if(amountText.getText().toString() != null){
+                        int amount = Integer.parseInt(amountText.getText().toString());
+                        String comment = commentText.getText().toString();
+                    }else{
+                        Toast.makeText(getApplicationContext(),"Please fill amount of bottles/cans", Toast.LENGTH_SHORT);
+                    }
+
+                }catch(Exception e){
+
+                }
+            }
+        });
     }
+
+
 }
